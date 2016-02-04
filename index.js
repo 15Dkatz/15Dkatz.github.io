59d9deb9c1eafc40077e1a0dcbef2139e4a3b6fc
davidkatz <15Dkatz@shcp.edu>
2016-02-04 01:14:40 -0800

    Created pitch detection. Need to make real time. Created quiz generator for random interval and bottom Note. Need to add play bottom Note sound functionality. Plenty improved though.

--- index.js
+++ index.js
@@ -4,20 +4,117 @@ micApp.controller("listenController", function($scope) {
 	var tuner = new Wad.Poly();
 	tuner.add(voice);
 
+	$scope.userNote="";
+
+	// var kill=false;
+
 	$scope.listen = function() {
+		// if (!kill===true) {
 		voice.play();
-
 		tuner.updatePitch();
-		var logPitch = function(){
-			console.log(tuner.pitch, tuner.noteName)
-			requestAnimationFrame(logPitch)
-		};
 
+		var logPitch = function(){
+
+			$scope.userNote=tuner.noteName;
+
+			console.log($scope.userNote, "userNote");
+
+			console.log(tuner.pitch, tuner.noteName);
+			// requestAnimationFrame(logPitch);
+		};
+		// tuner.stopUpdatingPitch();
 		logPitch();
+		// }
 	}
 
 	$scope.stopListen = function() {
-		tuner.stopUpdatingPitch;
+		// tuner.stopUpdatingPitch();
+		// stop = 0;
+		// kill=true;
+		tuner.stop();
+		voice.stop();
+		tuner.stopUpdatingPitch();
 	}
 
+
+
+	// var noteNames = ['f3', 'f']
+	$scope.bottomNote = undefined;
+	$scope.topNote = undefined;
+	$scope.intervalLength = undefined;
+
+	$scope.generateInterval = function() {
+		//pick a random number 33-45, or F3 to F4.
+		//convert that number into a teoria key or note value.
+		//make that note playable (sound) with another function.
+		var randKeyNum = Math.floor(Math.random()*12)+33;
+
+		var randKeyTop = randKeyNum+(Math.floor(Math.random()*12));
+		
+		//debugging
+		console.log(randKeyNum, "rkm");
+
+		var noteFromKey = teoria.note.fromKey(randKeyNum);
+		$scope.bottomNote = noteFromKey.name() + noteFromKey.accidental() + noteFromKey.octave();
+
+		var topNoteFromTop = teoria.note.fromKey(randKeyTop);
+		$scope.topNote = topNoteFromTop.name() + topNoteFromTop.accidental() + topNoteFromTop.octave();
+
+		//debugging
+		console.log($scope.bottomNote, $scope.topNote, "n->t")
+		console.log(teoria.interval.between(noteFromKey, topNoteFromTop).toString(), 'i');
+
+		// $scope.bottomNote = bottomNote;
+		$scope.intervalLength=teoria.interval.between(noteFromKey, topNoteFromTop).toString();
+
+	}
+
+	$scope.scoreCount=0;
+
+
+	//need to include note playing.
+
+	$scope.checkAnswer = function() {
+		//check if sung topNote = the desired interval.
+		//use teoria.js interval method.
+
+
+		//really check if note letter, and note accidental is the same.
+
+
+		//taking off ending nums.
+		var uNote = $scope.userNote.toLowerCase().substr(0, $scope.userNote.length-1);
+		var tNote = $scope.topNote.substr(0, $scope.topNote.length-1);
+
+		console.log(uNote, tNote, "un compares to tn");
+
+		if (teoria.note(uNote).chroma() === teoria.note(tNote).chroma()) {
+			$scope.scoreCount++;
+		} else {
+			//resetting high score Code here.
+
+			console.log("Try again!");
+		}
+
+	} 
+
+	$scope.playBottomNote = function() {
+
+		//play battom Note...
+	}
+
+
+	//use teoria.js to pick a bottomNote from C3-C4.
+	//allow user to play note using wad.
+
+	//display the sung/played note.
+
+
+
+
 })
+
+
+
+//add Play bottomNote function.
+//make this real time.
